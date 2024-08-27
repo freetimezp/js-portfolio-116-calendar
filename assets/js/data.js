ed6d6c54e30640b268caa6c2a937f6061ee8e4c6
freetimezp <kraftukrnet@ukr.net>
2024-08-27 23:02:45 +0300

    03. js-portfolio-116-calendar. GSAP animation.

---
+++ assets/js/data.js
@@ -0,0 +1,84 @@
+const data = [
+    {
+        May: [
+            {
+                img: "https://cdn.pixabay.com/photo/2018/01/04/07/59/salt-harvesting-3060093_1280.jpg",
+                h1: "Evolution Art",
+                copy: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem itaque vero vel nam quidem maiores quia facilis voluptatem eligendi dolorem.",
+                linkLabel: "Explore More",
+                lincSrc: "https://www.example.com/evolution-art",
+            },
+            {
+                img: "https://cdn.pixabay.com/photo/2017/07/31/11/44/laptop-2557571_1280.jpg",
+                h1: "Work Art",
+                copy: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem itaque vero vel nam quidem maiores quia facilis voluptatem eligendi dolorem.",
+                linkLabel: "Discover More",
+                lincSrc: "https://www.example.com/work-art",
+            },
+            {
+                img: "https://cdn.pixabay.com/photo/2021/03/02/13/04/laptop-6062423_1280.jpg",
+                h1: "Hard Learning",
+                copy: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem itaque vero vel nam quidem maiores quia facilis voluptatem eligendi dolorem.",
+                linkLabel: "See More",
+                lincSrc: "https://www.example.com/hard-learning",
+            },
+            {
+                img: "https://cdn.pixabay.com/photo/2015/05/31/15/14/woman-792162_1280.jpg",
+                h1: "Time Coffee",
+                copy: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem itaque vero vel nam quidem maiores quia facilis voluptatem eligendi dolorem.",
+                linkLabel: "Want More",
+                lincSrc: "https://www.example.com/time-coffee",
+            },
+        ],
+        June: [
+            {
+                img: "https://cdn.pixabay.com/photo/2015/12/07/10/58/architect-1080592_1280.jpg",
+                h1: "Happy Art",
+                copy: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem itaque vero vel nam quidem maiores quia facilis voluptatem eligendi dolorem.",
+                linkLabel: "Learn More",
+                lincSrc: "https://www.example.com/happy-art",
+            },
+        ],
+        July: [
+            {
+                img: "https://cdn.pixabay.com/photo/2017/10/10/21/49/blogger-2838945_1280.jpg",
+                h1: "How to start this?",
+                copy: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem itaque vero vel nam quidem maiores quia facilis voluptatem eligendi dolorem.",
+                linkLabel: "Read More",
+                lincSrc: "https://www.example.com/ho-to-start-this",
+            },
+            {
+                img: "https://cdn.pixabay.com/photo/2016/06/12/20/27/macro-1452987_1280.jpg",
+                h1: "Mech",
+                copy: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem itaque vero vel nam quidem maiores quia facilis voluptatem eligendi dolorem.",
+                linkLabel: "View Gallery",
+                lincSrc: "https://www.example.com/mech",
+            },
+            {
+                img: "https://cdn.pixabay.com/photo/2024/05/19/08/39/work-8772016_1280.jpg",
+                h1: "Industry",
+                copy: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem itaque vero vel nam quidem maiores quia facilis voluptatem eligendi dolorem.",
+                linkLabel: "Check It",
+                lincSrc: "https://www.example.com/industry",
+            },
+        ],
+        August: [
+            {
+                img: "https://cdn.pixabay.com/photo/2018/03/22/02/37/email-3249062_1280.png",
+                h1: "Post work",
+                copy: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem itaque vero vel nam quidem maiores quia facilis voluptatem eligendi dolorem.",
+                linkLabel: "Explore More",
+                lincSrc: "https://www.example.com/post-work",
+            },
+            {
+                img: "https://cdn.pixabay.com/photo/2015/11/07/08/49/hand-1030563_1280.jpg",
+                h1: "Camera",
+                copy: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem itaque vero vel nam quidem maiores quia facilis voluptatem eligendi dolorem.",
+                linkLabel: "Discover",
+                lincSrc: "https://www.example.com/camera",
+            },
+        ],
+    }
+];
+
+export default data;
